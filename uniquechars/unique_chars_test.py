8c02bede4dd45a48bad8e5c5feb2ab0c6c0a24f4
kellykeller1213 <keller.f.peter@gmail.com>
2017-10-06 11:35:51 +0200

    feat(uniquechars): finished unittest

---
+++ uniquechars/unique_chars_test.py
@@ -0,0 +1,17 @@
+import unittest
+from unique_chars import *
+
+class TestFile(unittest.TestCase):
+    
+    def test_not_repeatable_characters(self):
+        self.assertTrue(unique_characters("slma"))
+
+    def test_repeatable_characters(self):
+        self.assertFalse(unique_characters("aaaaa"))
+
+    def test_no_word_to_examine(self):
+        self.assertFalse(unique_characters(""))
+
+    
+if __name__ == '__main__':
+    unittest.main()
