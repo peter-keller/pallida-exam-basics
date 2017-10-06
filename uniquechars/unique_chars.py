bf79af70868ffdc62d54d2b37a1ee83d2c172335
kellykeller1213 <keller.f.peter@gmail.com>
2017-10-06 11:35:22 +0200

    feat(uniquechars): function doesn't work, takes only first item from list

--- uniquechars/unique_chars.py
+++ uniquechars/unique_chars.py
@@ -1,6 +1,16 @@
 # Create a function called `unique_characters` that takes a string as parameter
 # and returns a list with the unique letters of the given string
 # Create basic unit tests for it with at least 3 different test cases
+
+
+def unique_characters(word):
+    letter_list = list(word)
+    new_list = []
+    for i in range(len(letter_list)):
+        for letter in word:
+            if letter[i] not in new_list:
+                new_list.append(letter[i])
+                return new_list
+
+
 print(unique_characters("anagram"))
-# Should print out:
-# ["n", "g", "r", "m"]
